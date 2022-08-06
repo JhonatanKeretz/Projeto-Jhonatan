b51458cd7f62041184cfd0f882d9c1d6e6e8ef59
Jhonatan Keretz <92455620+JhonatanKeretz@users.noreply.github.com>
2022-08-06 07:31:42 -0300

    Add files via upload

---
+++ src/main.ts
@@ -0,0 +1,12 @@
+import {createApp} from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import axios from 'axios';
+
+axios.defaults.baseURL = 'http://localhost:8000/api/';
+axios.defaults.withCredentials = true;
+
+createApp(App).use(store).use(router).mount('#app')
+
+/// <reference path="globals/c3/index.d.ts" />
---
+++ src/model/product.ts
@@ -0,0 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+export class Product {
+    constructor(
+        id: number,
+        title: string,
+        description: string,
+        image: string,
+        price: number
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
+    ) {
+    }
+}
